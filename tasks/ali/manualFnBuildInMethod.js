aa88bc94eaaf26dac05180832f261f80c5961229
l1mus <alinesezcore@gmail.com>
2026-03-09 11:42:11 +0700

    refactor: remove the data variable and move it to index.js

--- tasks/ali/manualFnBuildInMethod.js
+++ tasks/ali/manualFnBuildInMethod.js
@@ -1,33 +1,6 @@
 // map
 // filter
 
-const data = [
-  {
-    id: 1,
-    name: "Ali Mustadji",
-    hobbies: [
-      "running",
-      "gaming",
-      "swimming",
-      "programming",
-      "soccer",
-      "basketball",
-    ],
-  },
-  { id: 2, name: "John Doe", hobbies: ["running", "programming", "sketting"] },
-  { id: 3, name: "John Takeuchi", hobbies: ["gaming", "fishing", "swimming"] },
-  {
-    id: 4,
-    name: "Sir Alex Ferguson ",
-    hobbies: ["running", "basketball", "coaching"],
-  },
-  {
-    id: 5,
-    name: "Cristiano Messi",
-    hobbies: ["running", "soccer", "swimming"],
-  },
-];
-
 export const manualBuildIFilter = (data, search) => {
   const result = [];
   for (let i = 0; i < data.length; i++) {
