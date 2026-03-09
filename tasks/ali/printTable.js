9d8a2034a0f0a3e4ccff18745f966ca5373c7129
l1mus <alinesezcore@gmail.com>
2026-03-09 13:39:52 +0700

    refactor: change return variable result  array of string to terminal logging multiplication number

--- tasks/ali/printTable.js
+++ tasks/ali/printTable.js
@@ -1,11 +1,8 @@
 export const cetakTabel = (data) =>{
-    console.log(typeof data)
-    const result = []
     if(typeof data !== "number" || data < 0){
         throw new Error("Input tidak valid , harus Nomor")
     }
     for(let i = 1 ; i <= 10 ; i++){
-        result.push(`${data} x ${i} = ${data*i}`)
+        console.log(`${data} x ${i} = ${data*i}`)
     }
-    return result
 }
