f0732273868b45062b2ebf2d24758582689d1736
anggavb <anggavb8@gmail.com>
2026-03-09 14:40:31 +0700

    fix: remove parameter for default value on task 1

--- tasks/angga/manual.js
+++ tasks/angga/manual.js
@@ -1,4 +1,4 @@
-export const manualMap = (array, callback, thisValue = undefined) => {
+export const manualMap = (array, callback) => {
   if (!Array.isArray(array)) throw new Error("Value must be Array"); // melakukan validasi pada data yang diterima harus berbentuk array
   if (typeof callback !== "function") throw new Error("Callback must be a function!"); // melakukan validasi pada parameter callback supaya yang diterima bersifat function
   const result = []; // membuat array kosong untuk menampung hasil map
@@ -6,22 +6,9 @@ export const manualMap = (array, callback, thisValue = undefined) => {
   // lakukan proses looping untuk proses perubahan data satu persatu
   for (let i = 0; i < array.length; i++) {
     if (i in array) { // check apakah urutan i ada pada index array
-      if (thisValue !== undefined && thisValue !== null) { // check apakah thisValue memiliki isi atau tidak
-        if (typeof thisValue !== "object") throw new Error("Default value should an Object!"); // validasi terlebih dahulu apakah thisValue adalah object karena harus bertipe object
-        
-        // membuat variable yang berisi Object yang di dalamnya sudah ada object dari thisValue
-        const context = Object(thisValue);
-        context.newFn = callback; // menambahkan function callback kedalam object
-    
-        // melakukan operasi maping ditambah adanya default value berbentuk 'this' didalamnya
-        result[i] = context.newFn(array[i], i, array);
-
-        // hapus fungsi tersebut supaya bersih
-        delete context.newFn;
-      } else {
-        // jika thisValue tidak ada maka gunakan operasi langsung dari callback function
-        result[i] = callback(array[i], i, array);
-      }
+      
+      // masukkan semua data dari kondisi callback function kedalam result
+      result[i] = callback(array[i], i, array);
     }
   }
 
@@ -29,7 +16,7 @@ export const manualMap = (array, callback, thisValue = undefined) => {
   return result; // function manualMap menghasilkan array terbaru dari variable result
 };
 
-export const manualFilter = (array, callback, thisValue = undefined) => {
+export const manualFilter = (array, callback) => {
   if (!Array.isArray(array)) throw new Error("Value must be Array"); // melakukan validasi pada data yang diterima harus berbentuk array
   if (typeof callback !== "function") throw new Error("Callback must be a function!"); // melakukan validasi pada parameter callback supaya yang diterima bersifat function
   const result = []; // membuat array kosong untuk menampung hasil filter
@@ -37,27 +24,10 @@ export const manualFilter = (array, callback, thisValue = undefined) => {
   // lakukan proses looping untuk proses perubahan data satu persatu
   for (let i = 0; i < array.length; i++) {
     if (i in array) { // check apakah urutan i ada pada index array
-      if (thisValue !== undefined && thisValue !== null) { // check apakah thisValue memiliki isi atau tidak
-        if (typeof thisValue !== "object") throw new Error("Default value should an Object!"); // validasi terlebih dahulu apakah thisValue adalah object karena harus bertipe object
-        
-        // membuat variable yang berisi Object yang di dalamnya sudah ada object dari thisValue
-        const context = Object(thisValue);
-        context.newFn = callback; // menambahkan function callback kedalam object
-    
-        // mengecek apakah callback function return true
-        if (context.newFn(array[i], i, array)) {
-          result[index] = array[i]; // jika true maka value urutan ke 'i' dimasukkan kedalam result
-          index++;
-        }
-
-        // hapus fungsi tersebut supaya bersih
-        delete context.newFn;
-      } else {
-        // jika thisValue tidak ada maka gunakan operasi langsung dari callback function
-        // mengecek apakah callback function return true
-        if (callback(array[i], i, array)) {
-          result[i] = array[i]; // jika true maka value urutan ke 'i' dimasukkan kedalam result
-        }
+      // mengecek apakah callback function return true
+      if (callback(array[i], i, array)) {
+        result[index] = array[i]; // jika true maka value urutan ke 'i' dimasukkan kedalam result
+        index++;
       }
     }
   }
