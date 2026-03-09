5437198f50165481dcecd8f6f831a112dda07857
l1mus <alinesezcore@gmail.com>
2026-03-09 14:44:43 +0700

    refactor: refactor  manualBuildInMap and manualBuildInFilter functions to have a CallbackFn parameter

--- tasks/ali/manualFnBuildInMethod.js
+++ tasks/ali/manualFnBuildInMethod.js
@@ -1,28 +1,32 @@
 // map
 // filter
 
-export const manualBuildIFilter = (data, search) => {
-  const result = [];
-  for (let i = 0; i < data.length; i++) {
-    let value = data[i];
-    for (let hobbie of value.hobbies) {
-      if (hobbie === search) {
-        result.push(value);
-      }
+const callbackFnFilter = (element,index,array) => {
+    if(element % 2 === 0){
+      return element
     }
+}  
+
+const callbackFnMap = (element,index,array) =>{
+  return element * 2
+}
+
+export const manualBuildInFilter = (data, callbackFn) => {
+  let result = []
+  for (let i = 0; i < data.length; i++) {
+    let value = callbackFn(data[i])
+    if(value === undefined) continue
+    result = [value, ...result]
   }
-  return result;
+  return result
 };
 
-export const manualBuildInMap = (data, ...infomation) => {
-  const result = [];
-  const map = new Map();
+export const manualBuildInMap = (data,callbackFn) => {
+      let result = []
   for (let i = 0; i < data.length; i++) {
-    let value = data[i];
-    for (let key of infomation) {
-      map.set(key, value[key]);
-    }
-    result.push(Object.fromEntries(map));
+    let value = callbackFn(data[i])
+    if(value === undefined) continue
+    result = [...result,value]
   }
-  return result;
+  return result
 };
