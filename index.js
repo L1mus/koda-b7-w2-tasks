4fd86677bdbe07f55e8326f3f606466b8b3d0619
anggavb <anggavb8@gmail.com>
2026-03-09 14:42:04 +0700

    fix: change order list by collection time

--- index.js
+++ index.js
@@ -39,140 +39,6 @@ console.log("===========================================");
     console.log(error.message);
   }
 })();
-console.log("===========================================");
-(function taskAngga() {
-  // Success Task 1a
-  try {
-    const calculator = {
-      num: 10,
-      count(number) {
-        return number * this.num; // 'this' merujuk ke objek kalkulator
-      }
-    };
-    const task1a = angga.manualMap([1,2,3], function (n) {
-      return this.count(n);
-    }, calculator);
-    console.log(task1a);
-  } catch (err) {
-    console.log(err.message);
-  }
-
-  // Failed 1: Param 1 is not an Array
-  try {
-    const task1a = angga.manualMap("not an array", (n) => {
-      return n * 10;
-    });
-    console.log(task1a);
-  } catch (err) {
-    console.log(err.message);
-  }
-  
-  // Failed 2: Param 2 is not a Function
-  try {
-    const task1a = angga.manualMap([1,2,3], "not a function");
-    console.log(task1a);
-  } catch (err) {
-    console.log(err.message);
-  }
-  
-  // Failed 3: Param 3 (optional) is not an Object
-  try {
-    const calculator = "try for error";
-    const task1a = angga.manualMap([1,2,3], (n) => {
-      return this.count(n);
-    }, calculator);
-    console.log(task1a);
-  } catch (err) {
-    console.log(err.message);
-  }
-
-  // Success Task 1b
-  try {
-    const condition = {
-      num: 2,
-      check(number) {
-        return number % this.num === 0; // 'this' merujuk ke objek kalkulator
-      }
-    };
-    const task1b = angga.manualFilter([1,2,3,4,5,6,7,8,9,10], function (n) {
-      return this.check(n);
-    }, condition);
-    console.log(task1b);
-  } catch (err) {
-    console.log(err.message);
-  }
-
-  // Failed 1: Param 1 is not an Array
-  try {
-    const task1b = angga.manualFilter("not an array", (n) => {
-      return n % 2 === 0;
-    });
-    console.log(task1b);
-  } catch (err) {
-    console.log(err.message);
-  }
-  
-  // Failed 2: Param 2 is not a Function
-  try {
-    const task1b = angga.manualFilter([1,2,3,4,5,6,7,8,9,10], "not a function");
-    console.log(task1b);
-  } catch (err) {
-    console.log(err.message);
-  }
-  
-  // Failed 3: Param 3 (optional) is not an Object
-  try {
-    const calculator = "try for error";
-    const task1b = angga.manualFilter([1,2,3,4,5,6,7,8,9,10], (n) => {
-      return this.count(n);
-    }, calculator);
-    console.log(task1b);
-  } catch (err) {
-    console.log(err.message);
-  }
-
-  // Success Task 2
-  try {
-    const task2 = angga.cetakTabel(3);
-    console.table(task2);
-  } catch (err) {
-    console.log(err.message);
-  }
-
-  // Failed Task 2
-  try {
-    const task2 = angga.cetakTabel("abc");
-    console.table(task2);
-  } catch (err) {
-    console.log(err.message);
-  }
-
-  // Failed Task 2
-  try {
-    const task2 = angga.cetakTabel(-1);
-    console.table(task2);
-  } catch (err) {
-    console.log(err.message);
-  }
-
-  // Success Task 3
-  try {
-    const task3a = angga.countArray([1,2,3,4,5]);
-    const task3b = angga.countArray([2,2,2,2,2]);
-    console.log(task3a);
-    console.log(task3b);
-  } catch (err) {
-    console.log(err.message);
-  }
-
-  // Failed Task 3
-  try {
-    const task3 = angga.countArray([1,2,3,4,"asd"]);
-    console.log(task3);
-  } catch (err) {
-    console.log(err.message);
-  }
-})();
 
 /**
  * Task Naufal
@@ -252,5 +118,105 @@ console.log("===========================================");
     console.log(error.message);
   }
   
+})();
+console.log("===========================================");
+(function taskAngga() {
+  // Success Task 1a
+  try {
+    const task1a = angga.manualMap([1,2,3], function (n) {
+      return n * 10;
+    });
+    console.log(task1a);
+  } catch (err) {
+    console.log(err.message);
+  }
+
+  // Failed 1: Param 1 is not an Array
+  try {
+    const task1a = angga.manualMap("not an array", (n) => {
+      return n * 10;
+    });
+    console.log(task1a);
+  } catch (err) {
+    console.log(err.message);
+  }
+  
+  // Failed 2: Param 2 is not a Function
+  try {
+    const task1a = angga.manualMap([1,2,3], "not a function");
+    console.log(task1a);
+  } catch (err) {
+    console.log(err.message);
+  }
+
+  // Success Task 1b
+  try {
+    const task1b = angga.manualFilter([1,2,3,4,5,6,7,8,9,10], function (n) {
+      return n % 2 === 0;
+    });
+    console.log(task1b);
+  } catch (err) {
+    console.log(err.message);
+  }
+
+  // Failed 1: Param 1 is not an Array
+  try {
+    const task1b = angga.manualFilter("not an array", (n) => {
+      return n % 2 === 0;
+    });
+    console.log(task1b);
+  } catch (err) {
+    console.log(err.message);
+  }
+  
+  // Failed 2: Param 2 is not a Function
+  try {
+    const task1b = angga.manualFilter([1,2,3,4,5,6,7,8,9,10], "not a function");
+    console.log(task1b);
+  } catch (err) {
+    console.log(err.message);
+  }
+
+  // Success Task 2
+  try {
+    const task2 = angga.cetakTabel(3);
+    console.table(task2);
+  } catch (err) {
+    console.log(err.message);
+  }
+
+  // Failed Task 2
+  try {
+    const task2 = angga.cetakTabel("abc");
+    console.table(task2);
+  } catch (err) {
+    console.log(err.message);
+  }
+
+  // Failed Task 2
+  try {
+    const task2 = angga.cetakTabel(-1);
+    console.table(task2);
+  } catch (err) {
+    console.log(err.message);
+  }
+
+  // Success Task 3
+  try {
+    const task3a = angga.countArray([1,2,3,4,5]);
+    const task3b = angga.countArray([2,2,2,2,2]);
+    console.log(task3a);
+    console.log(task3b);
+  } catch (err) {
+    console.log(err.message);
+  }
+
+  // Failed Task 3
+  try {
+    const task3 = angga.countArray([1,2,3,4,"asd"]);
+    console.log(task3);
+  } catch (err) {
+    console.log(err.message);
+  }
 })();
 console.log("===========================================");
